20f0fe331a1d53f1a481e6df1eeeffd96b26c4bb
Aiden Kampman <aidenshanekampman@gmail.com>
2022-02-09 12:13:15 -0700

    Finished problem 8

--- lab.js
+++ lab.js
@@ -106,6 +106,10 @@ let passingScore = 7
 */
 
 //CODE HERE
+while (score < passingScore){
+  console.log('Your score is not high enough')
+score++
+}
 
 ////////// INTERMEDIATE PROBLEMS //////////
 
